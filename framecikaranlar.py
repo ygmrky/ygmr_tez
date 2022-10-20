314c7dbbd7382a6a83af4eefaf88fee0fc7f209e
ygmrky <95273346+ygmrky@users.noreply.github.com>
2022-10-21 00:03:38 +0300

    Add files via upload

--- framecikaranlar.py
+++ framecikaranlar.py
@@ -1,30 +1,28 @@
 import os
-
 import cv2
 
-video_path = 'C:/Users/yagmur.kaya/PycharmProjects/ygmr_project1/Framextract2folder/inputP'
+video_path = 'C:/Users/yagmur.kaya/PycharmProjects/ygmr_tez/Framextract2folder/inputP'
 
-output_path = 'C:/Users/yagmur.kaya/PycharmProjects/ygmr_project1/Framextract2folder/outputP'
+output_path = 'C:/Users/yagmur.kaya/PycharmProjects/ygmr_tez/Framextract2folder/outputP'
 
 input_file_list = os.listdir(video_path)
-
+print("input_file_list listele: ", *input_file_list, sep="\n")
 sep = '.'
-print(input_file_list)
 
 for i in input_file_list:
-    print(i)
-    iyi = i.split(sep, 1)[0]
-    print("yeni iiiimiz", iyi)
-    salih = os.path.join(output_path, iyi)
-    print(salih)
-   # os.mkdir(salih)
-    if not os.path.exists(salih):
-        os.mkdir(salih)
+    print("her bir klasör adı:", i)
+    klasor_isim = i.split(sep, 1)[0]
+    print("klasör ismi .dan ayrılmış hali:", klasor_isim)
+    yeni_path = os.path.join(output_path, klasor_isim)
+    print("image isminden oluşan yeni/son dosya yolu: ", yeni_path)
+   # os.mkdir(yeni_path)
+    if not os.path.exists(yeni_path):
+        os.mkdir(yeni_path)
     else:
-        print("Kıf kıf o klasör zaten var ki")
+        print("Klasör zaten var, oluşturmaya gerek yok")
 
     yolyolu = os.path.join(video_path, i)
-    print("yol yolu:", yolyolu)
+    print("input yolu:", yolyolu)
     cap = cv2.VideoCapture(yolyolu)
     index = 0
 
@@ -35,36 +33,10 @@ for i in input_file_list:
             index += 1
             if index % 24 != 0:
                 continue
-            cv2.imwrite(salih + '/' + 'Frame_' + str(index) + '.png', frame)
+            cv2.imwrite(yeni_path + '/' + 'Frame_' + str(index) + '.png', frame)
         else:
             break
     cap.release()
 
 
-'''
 
-
-for items in list:
-
-path = os.path.join(output_path, items)
-
-pwdpath)
-
-
-
-from pathlib import Path
-
-
-dir = '/path/to/some/file.txt'
-
-print(Path(dir).stem)
-
-
-
-sep = '...'
-
-stripped = text.split(sep, 1)[0]
-
-
-
-'''
